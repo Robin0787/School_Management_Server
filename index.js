e3dee8d67713aa83e29c6b08b89e1c25a83a5583
Robin <mohammadrobin636@gmail.com>
2023-09-15 02:42:59 +0600

    Added instuctors stats API

--- index.js
+++ index.js
@@ -7,20 +7,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  {
-    name: "Rahim",
-    age: "40",
-  },
-  {
-    name: "Liton",
-    age: "37",
-  },
-  {
-    name: "Abdul",
-    age: "45",
-  },
-];
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nxzja4k.mongodb.net/?retryWrites=true&w=majority`;
@@ -40,12 +28,13 @@ async function run() {
     await client.connect();
 
     const database = client.db("School_Management");
+    const approvedUserCollection = database.collection("Approved_Users");
+    const currentStudentsCollection = database.collection("current-students");
     const subjectCollection = database.collection("subjects");
     const instructorRequests = database.collection("instructor-requests");
     const studentsRequest = database.collection("student-requests");
     const ApprovedStudents = database.collection("approved-students");
     const RejectedStudents = database.collection("rejected-students");
-    const approvedUserCollection = database.collection("Approved_Users");
 
     // ----------- GET ----------- GET ----------- GET ----------- GET ----------- //
     // Getting all subject according to className
@@ -65,15 +54,14 @@ async function run() {
 
     // Getting all approved students
     app.get("/approved-students", async (req, res) => {
-      const result = await ApprovedStudents
-        .aggregate([
-          {
-            $group: {
-              _id: { $toLower: "$className" },
-              students: { $push: "$$ROOT" },
-            },
+      const result = await ApprovedStudents.aggregate([
+        {
+          $group: {
+            _id: { $toLower: "$className" },
+            students: { $push: "$$ROOT" },
           },
-        ]).toArray();
+        },
+      ]).toArray();
 
       const groupedData = {};
       result.forEach((item) => {
@@ -81,7 +69,7 @@ async function run() {
       });
       res.send(groupedData);
     });
-    
+
     // Getting all students request
     app.get("/students-request", async (req, res) => {
       const result = await studentsRequest
@@ -103,10 +91,59 @@ async function run() {
     });
 
     // Getting all instructors request
-    app.get('/instructors-request', async (req, res) => {
+    app.get("/instructors-request", async (req, res) => {
       const requests = await instructorRequests.find().toArray();
       res.send(requests);
-    })
+    });
+
+    // Getting all the current students
+    app.get("/get-current-students", async (req, res) => {
+      const result = await currentStudentsCollection
+        .aggregate([
+          {
+            $sort: { roll: 1 },
+          },
+          {
+            $group: {
+              _id: { $toLower: "$class" },
+              students: { $push: "$$ROOT" },
+            },
+          },
+        ])
+        .toArray();
+
+      const groupedData = {};
+      result.forEach((item) => {
+        groupedData[item._id] = item.students;
+      });
+      res.send(groupedData);
+    });
+
+    // Getting Instructor Stats
+    app.get("/get-instructor-stats", async (req, res) => {
+      const collectionNames = [
+        "current-students",
+        "student-requests",
+        "approved-students",
+        "rejected-students",
+      ];
+      const collectionStats = {};
+
+      const promises = collectionNames.map(async (collectionName) => {
+        const collection = database.collection(collectionName);
+        const count = await collection.countDocuments();
+        collectionStats[collectionName] = count;
+      });
+
+      try {
+        await Promise.all(promises);
+        res.send(collectionStats);
+      } 
+      catch (error) {
+        console.error("Error counting documents:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
 
     // ----------- POST ----------- POST ----------- POST ----------- POST ----------- //
     // Storing Instructors Sign Up request
@@ -124,10 +161,12 @@ async function run() {
     });
 
     // Storing approved students
-    app.post('/store-approved-student/:id', async(req, res) => {
+    app.post("/store-approved-student/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await studentsRequest.updateOne(query, {$set: {status: 'approved'}});
+      const query = { _id: new ObjectId(id) };
+      const result = await studentsRequest.updateOne(query, {
+        $set: { status: "approved" },
+      });
       const student = await studentsRequest.findOne(query);
       const result1 = await approvedUserCollection.insertOne(student);
       const result2 = await ApprovedStudents.insertOne(student);
@@ -135,25 +174,31 @@ async function run() {
       res.send(result3);
     });
 
-
-
+    // Storing Current Students
+    app.post("/store-current-student", async (req, res) => {
+      const studentInfo = req.body;
+      const result = await currentStudentsCollection.insertOne(studentInfo);
+      res.send(result);
+    });
 
     // ----------- PUT ----------- PUT ----------- PUT ----------- PUT ----------- //
 
     // ----------- PATCH ----------- PATCH ----------- PATCH ----------- PATCH ----------- //
 
     // ----------- DELETE ----------- DELETE ----------- DELETE ----------- DELETE ----------- //
-    
+
     // Rejecting Student Request
-    app.delete('/reject-student-request/:id', async (req, res) => {
+    app.delete("/reject-student-request/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const update = await studentsRequest.updateOne(query, {$set: {status: 'rejected'}});
+      const query = { _id: new ObjectId(id) };
+      const update = await studentsRequest.updateOne(query, {
+        $set: { status: "rejected" },
+      });
       const student = await studentsRequest.findOne(query);
       const storeStudent = await RejectedStudents.insertOne(student);
       const result = await studentsRequest.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
